d68d40a8d72486248f2c6d5107bd2d0c471a7e7c
Alessandro Magnani <alessandromagnani17@gmail.com>
2025-03-24 10:35:16 +0100

    Initial Commit

---
+++ KneeSurgeryPlanner/KneeSurgeryPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KneeSurgeryPlannerApp.swift
+//  KneeSurgeryPlanner
+//
+//  Created by Alessandro Magnani on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct KneeSurgeryPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KneeSurgeryPlannerTests/KneeSurgeryPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  KneeSurgeryPlannerTests.swift
+//  KneeSurgeryPlannerTests
+//
+//  Created by Alessandro Magnani on 24/03/25.
+//
+
+import Testing
+@testable import KneeSurgeryPlanner
+
+struct KneeSurgeryPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
