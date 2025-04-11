7a5d4e21955f075c2ce1be1badbcadf214138f71
Alessandro Magnani <alessandromagnani17@gmail.com>
2025-04-11 20:03:54 +0200

    refactor: define clear rendering enums for UI state management

---
+++ Utils/Rendering/RenderingEnums.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// Modalità di rendering del modello 3D
+public enum RenderingMode {
+    case solid              // Rendering solido standard
+    case wireframe          // Solo wireframe
+    case solidWithWireframe // Solido con overlay wireframe
+}
